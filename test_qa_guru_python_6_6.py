cb1f9817bcfbd2ebde00ea0188958230f7e948e3
Pavel Milyukov <milson7017@yandex.ru>
2023-06-23 20:50:40 +0300

    complete

--- test_qa_guru_python_6_6.py
+++ test_qa_guru_python_6_6.py
@@ -98,7 +98,6 @@ def go_to_companyname_homepage(page_url):
     actual_result = readable_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
-
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = readable_function(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
